8de4f9eb1033750088cfd77bc203625087fd2e6f
Piotr Krysiak <plotrekhqpl@gmail.com>
2023-02-28 21:58:46 +0100

    Initial project cleanup & install tailwind

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="bg-red-500 underline text-white aspect-square w-1/2">This is an about page</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
