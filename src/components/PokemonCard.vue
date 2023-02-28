a0746cc81a8587291d642dedddd9ea99113837d6
Piotr Krysiak <plotrekhqpl@gmail.com>
2023-02-28 23:43:49 +0100

    Simple fetch pokemon API

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  name: 'PokemonCard',
+  props: {
+    pokemonName: String,
+    pokemonUrl: String
+  }
+}
+</script>
+<template>
+  <div
+    class="aspect-square p-10 rounded-2xl bg-slate-400 shadow-2xl hover:shadow-sm hover:bg-slate-400/80"
+  >
+    <h1>{{ pokemonName }}</h1>
+    <img :src="pokemonUrl" alt="pokemon" />
+  </div>
+</template>
+
+<style lang=""></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+import { onMounted, ref, computed } from 'vue'
+import PokemonCard from '@/components/PokemonCard.vue'
+
+interface Pokemon {
+  name: string
+  url: string
+}
+
+const pokemonList = ref<Pokemon[]>([])
+
+const getPokemon = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const data = await res.json()
+  console.log(data)
+  pokemonList.value = data.results
+}
+
+//const jsonStringify = computed(() => JSON.stringify(pokemonList.value, null, 2))
+
+onMounted(() => {
+  getPokemon()
+})
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="bg-red">This is an about page</h1>
+    <div class="grid grid-cols-3 gap-4">
+      <PokemonCard
+        v-for="pokemon in pokemonList"
+        :key="pokemon.name"
+        :pokemonName="pokemon.name"
+        :pokemonUrl="pokemon.url"
+      />
+    </div>
   </div>
 </template>
