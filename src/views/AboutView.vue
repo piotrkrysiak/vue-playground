95533aab92dbcb4e4ef1a540fd23d260ee62fe0f
Piotr Krysiak <plotrekhqpl@gmail.com>
2023-03-01 17:45:23 +0100

    loading and error state

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,11 +9,21 @@ interface Pokemon {
 }
 
 const pokemonList = ref<Pokemon[]>([])
+const error = ref<string | null>(null)
 
 const getPokemon = async () => {
-  const res = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json')
-  pokemonList.value = await res.json()
+  try {
+    const res = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json')
+    pokemonList.value = await res.json()
+  } catch (err) {
+    let errorMessage = 'Failed to do something exceptional'
+    if (err instanceof Error) {
+      errorMessage = err.message
+    }
+    error.value = errorMessage
+  }
 }
+
 const pokemonList21 = computed(() => {
   return pokemonList.value.slice(0, 21)
 })
@@ -27,6 +37,7 @@ onMounted(() => {
   <div class="about">
     <h1 class="bg-red">This is an about page</h1>
     <div
+      v-if="pokemonList21.length > 0 && !error"
       class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-5 md:px-10 2xl:px-0 gap-4 max-w-7xl mx-auto"
     >
       <PokemonCard
@@ -36,6 +47,12 @@ onMounted(() => {
         :name="pokemon.name"
         :url="pokemon.image"
       />
+    </div>
+    <div v-else-if="error">
+      <h1>{{ error }}</h1>
+    </div>
+    <div v-else>
+      <h1>Loading...</h1>
     </div>
   </div>
 </template>
